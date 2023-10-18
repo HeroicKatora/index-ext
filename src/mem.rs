3f89ef20ebd5655fb6bb45b1c87db24d2b92c5d0
Andreas Molzer <andreas.molzer@gmx.de>
2023-10-18 23:48:54 +0200

    Rewrite mem from feedback / usage

--- src/mem.rs
+++ src/mem.rs
@@ -1,19 +1,42 @@
 //! Integer wrappers that are constrained to the range of `usize` or `isize`.
 //!
-//! This module defines a number of wrappers around the basic integer types which ensure that the
-//! stored value can be trivially converted to a `usize` without loss of data. That is these types
-//! offer a getter that converts the number with `as usize` but otherwise they are ABI compatible
-//! with their underlying integer type.
+//! This module defines a number of wrappers around underlying basic integer types which ensure
+//! that the value fits both within `usize` (respectively `isize`) as well as the basic integer
+//! type, without loss of data.
+//!
+//! Additionally, the types are optimized for size. That is, the types occupy the minimum size of
+//! both the underlying integer and the respective pointer sized alternative. This ensure that
+//! platform-compatible code is easier to write without wasting memory for the data representation
+//! in the process.
 //!
 //! # Usage
 //!
-//! These are best used in places where the ABI or layout of the type is important but there is a
-//! security risk associated with having to write (fallible) conversions all the time. Consider the
-//! case of a matrix where dimensions are stored as `u32` for compatibility reasons. We would now
-//! like to allocate a buffer for it which requires calculating the number of elements as a `u64`
-//! and then convert to `usize`. However, no matter in which number type you intend to store the
-//! result you lose semantic meaning because there is no 'proof' attached to the value that it will
-//! also fit into the other value range.
+//! Imagine some interface defining indices to be in the range of a `u64`. A 32-bit platform
+//! implementing this interface may be torn between using `u64` for the intent, as well as its own
+//! `isize` for the smaller representation. The corresponding equivalent combines both:
+//!
+//! ```
+//! use core::mem::size_of;
+//! use index_ext::mem::Umem64;
+//!
+//! assert!(size_of::<Umem64>() <= size_of::<usize>());
+//! assert!(size_of::<Umem64>() <= size_of::<u64>());
+//! ```
+//!
+//! Keep in mind the types are most useful for representing values in and relative to your own
+//! program's memory. In the interface you would generally prefer to declare argument as the
+//! specified platform independent underlying integer (e.g. `u64` above). In these cases, there is
+//! however a security risk associated with delaying the (fallible) conversions to your platform's
+//! capabilities. Firstly, implicit loss of precision when using `as` may occur on some
+//! distributions but not others which may result in incomplete test coverage missing a bug.
+//! Secondly, utilizing fallible conversion at the site of use creates many error paths. You might
+//! prefer converting to `Umem64` immediately.
+//!
+//! Consider the case of a matrix where dimensions are stored as `u32` for compatibility reasons.
+//! We would now like to allocate a buffer for it which requires calculating the number of elements
+//! as a `u64` and then convert to `usize`. However, no matter in which number type you intend to
+//! store the result you lose semantic meaning because there is no 'proof' attached to the value
+//! that it will also fit into the other value range.
 //!
 //! ```
 //! use index_ext::mem::Umem64;
@@ -31,16 +54,34 @@
 //! let matrix = vec![0; length.get()];
 //! # Some(()) }
 //! ```
+struct Choice<const C: bool>;
+
+trait Impl<T> {
+    type Inner;
+}
+
 macro_rules! lossless_integer {
     (
         $(#[$attr:meta])*
         $sizet:ident $str_name:literal
         pub struct $name:ident ($under:ty)
     ) => {
+        impl Impl<$name> for Choice<true> {
+            type Inner = $under;
+        }
+        impl Impl<$name> for Choice<false> {
+            type Inner = $sizet;
+        }
+
         $(#[$attr])*
         #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
         #[repr(transparent)]
-        pub struct $name($under);
+        pub struct $name(
+            <
+                Choice<{core::mem::size_of::<$under>() < core::mem::size_of::<$sizet>()}>
+                as Impl<$name>
+            >::Inner
+        );
 
         impl $name {
             /// Wrap an integer if its value can be losslessly converted to `
@@ -48,7 +89,10 @@ macro_rules! lossless_integer {
             /// `.
             pub fn new(val: $under) -> Option<Self> {
                 if <$sizet as core::convert::TryFrom<_>>::try_from(val).is_ok() {
-                    Some($name(val))
+                    // Potentially no-op, potentially a cast. The macro and type deduction makes
+                    // sure either $under or $sizet is utilized as the result. Both are correct by
+                    // the input argument and the `try_from`.
+                    Some($name(val as _))
                 } else {
                     None
                 }
@@ -63,7 +107,19 @@ macro_rules! lossless_integer {
 
             /// Get the inner value in the original type.
             pub fn into_inner(self) -> $under {
-                self.0
+                self.0 as $under
+            }
+        }
+
+        impl From<$name> for $sizet {
+            fn from(val: $name) -> $sizet {
+                val.get()
+            }
+        }
+
+        impl From<$name> for $under {
+            fn from(val: $name) -> $under {
+                val.into_inner()
             }
         }
     };
